5904a06334877d6a856b1c3becacd5590a7bfa4f
Tarek Ziade <tarek@ziade.org>
2024-03-11 04:00:45 -0700

    script to create the clean json dataset

---
+++ create_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from datasets import Dataset
+
+
+df = pd.read_csv("clean_data.csv")
+dataset = Dataset.from_pandas(df)
+
+
+def filter_empty_texts(example):
+    return example["text"] is not None and example["text"].strip() != ""
+
+
+dataset = dataset.filter(filter_empty_texts)
+
+dataset.to_json("profanity-check.json")
